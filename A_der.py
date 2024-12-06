0ddcb128919563b4f756b33ecf68faa4cd005cca
Uellah <borchik121177@mail.ru>
2024-12-07 02:37:12 +0300

    Index error correction

--- A_der.py
+++ A_der.py
@@ -22,67 +22,80 @@ Lx = 1
 Ly =  1
 hx = Lx / (n * (p - 1))  # Шаг сетки по x
 hy = Ly / (N_y - 1)  # Шаг сетки по y
-grid = np.ones((N_y, N_x))  # Общая сетка всех пластин
+grid = np.ones((N_x, N_y))  # Общая сетка всех пластин
+grid[0, :] = [1, 2, 3]
+grid[1, :] = [2, 3, 4]
+grid[2, :] = [3, 4, 5]
+grid[3, :] = [4, 5, 6]
+grid[4, :] = [5, 6, 7]
+grid[5, :] = [6, 7, 8]
 
+print(hx)
 OUT_FILE.write_numpy_to_csv(grid)
 
-print(grid)
+# print(grid)
 class A:
     def __init__(self, hx, hy):
         self.hx = hx
         self.hy = hy
 
-    def second_x_der(self, u, j, i):
-        res = (u[i][j-1] - 2 * u[i][j] + u[i][j-1])/np.power(self.hy, 2)
+    def second_y_der(self, u, i, j):
+        res = (u[i][j-1] - 2 * u[i][j] + u[i][j+1])/np.power(self.hy, 2)
         return res
 
-    def second_y_der(self, u, j, i):
+    def second_x_der(self, u, i, j):
         res = (u[i-1][j] - 2 * u[i][j] + u[i+1][j])/np.power(self.hx, 2)
         return res
 
-    def puasson(self, u, j, i):
-        return self.second_x_der(u, j, i) + self.second_y_der(u, i, j)
+    def puasson(self, u, i, j):
+        return self.second_x_der(u, i, j) + self.second_y_der(u, i, j)
 
     def left_bound_exter(self, u, j):
-        first = (u[j][0] - u[j][1])/np.power(self.hx, 2)
+        first = (u[0][j] - u[1][j])/np.power(self.hx, 2)
         second = self.second_y_der(u, 0, j)/2
-        third = (h(u_l) - h(u[j][0]))/self.hx/lamda
+        third = (h(u_l) - h(u[0][j]))/self.hx/lamda
         return first - second - third
 
-    def right_bound_exter(self, u, j, i):
+    def right_bound_exter(self, u, i, j):
         first = (u[i][j] - u[i-1][j])/np.power(self.hx, 2)
-        second = self.second_y_der(u, j, i)/2
+        second = self.second_y_der(u, i, j)/2
         third = (h(u_r) - h(u[i][j]))/self.hx/lamda
         return first - second - third
 
-    def left_bound_inner(self, u, j, i):
+    def left_bound_inner(self, u, i, j):
         first = (u[i][j] - u[i - 1][j]) / np.power(self.hx, 2)
-        second = self.second_y_der(u, j, i) / 2
+        second = self.second_y_der(u, i, j) / 2
         third = (h(u[i+1][j])- h(u[i][j])) / self.hx / lamda
         return first - second - third
 
-    def right_bound_inner(self, u, j, i):
+    def right_bound_inner(self, u, i, j):
         first = (u[i][j] - u[i + 1][j]) / np.power(self.hx, 2)
-        second = self.second_y_der(u, j, i) / 2
+        second = self.second_y_der(u, i, j) / 2
         third = (h(u[i-1][j]) - h(u[i][j])) / self.hx / lamda
         return first - second - third
 
     def apply_operator(self):
-        res = np.zeros((N_y, N_x))
-        print(N_x, N_y)
-        for i in range(N_x):
-            res[0][i] = grid[0][i]
-            res[N_y-1][i] = grid[N_y-1][i]
+        res = np.zeros((N_x, N_y))
 
-        for i in range(1, N_y-1):
-            res[i][0] = self.left_bound_exter(grid, i)
-            res[i][N_x-1] = self.right_bound_exter(grid, N_x-1, i)
+        for j in range(1, N_y - 1):
+            res[0][j] = self.left_bound_exter(grid, j)
+            res[N_x - 1][j] = self.right_bound_exter(grid, N_x - 1, j)
 
+        for i in range(1, N_x - 1):
+            for j in range(1, N_y - 1):
+                if i % p == 0:
+                    res[i][j] = self.left_bound_inner(grid, i, j)
+                    break
+                if i % p == p - 1:
+                    res[i][j] = self.right_bound_inner(grid, i,  j)
+                    break
+                res[i][j] = self.puasson(grid, i, j)
         print(res)
-        print(h(u_l) - h(1)/lamda/hx)
-        # for i in range(1, N_x - 1):
-        #     if i
-        #     for j in range(1, N_y - 1):
+        return res
+
+# print(grid)
+# S = A(hx, hy)
+# S.apply_operator()
 
 
 
@@ -92,7 +105,7 @@ class A_der:
         self.hy = hy
 
     def second_y_der(self, u, i, j):
-        res = (u[i][j - 1] - 2 * u[i][j] + u[i][j - 1]) / np.power(self.hy, 2)
+        res = (u[i][j - 1] - 2 * u[i][j] + u[i][j + 1]) / np.power(self.hy, 2)
         return res
 
     def second_x_der(self, u, i, j):
@@ -125,6 +138,27 @@ class A_der:
         third = h_der(u[i - 1][j]) / (self.hx * lamda) * u_der[i - 1][j]
         return first - second - third - u_der[i + 1][j] / np.power(self.hx, 2)
 
-A_ = A(hx, hy)
-res = A_.apply_operator()
-print(res)
+    def apply_operator(self):
+        res = np.zeros((N_x, N_y))
+
+        for j in range(1, N_y - 1):
+            res[0][j] = self.left_bound_exter(grid, grid, j)
+            res[N_x - 1][j] = self.right_bound_exter(grid, grid, N_x - 1, j)
+
+        for i in range(1, N_x - 1):
+            for j in range(1, N_y - 1):
+                if i % p == 0:
+                    res[i][j] = self.left_bound_inner(grid, grid, i, j)
+                    break
+                if i % p == p - 1:
+                    res[i][j] = self.right_bound_inner(grid, grid, i, j)
+                    break
+                res[i][j] = self.puasson(grid, i, j)
+
+        print(res)
+
+# print(grid)
+S = A_der(hx, hy)
+S.apply_operator()
+S = A(hx, hy)
+S.apply_operator()
